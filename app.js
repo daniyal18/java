67104f94aa0b052945c909bbf3717f704115fa68
daniyal18 <daniyalnoman143@gmail.com>
2021-01-14 18:08:09 -0800

    complete

---
+++ app.js
@@ -0,0 +1,201 @@
+
+             //Changing Case
+
+
+// var start = prompt("enter any thing")
+// var now = start.toLocaleUpperCase();
+// alert(now)
+
+
+
+// var two =  prompt("enter any thing")
+// var second = two.toLocaleLowerCase();
+// alert(second)
+
+            
+               //String:measuring length     
+
+
+// var model = prompt("enter model of your favourite mobile")
+// var length = model.length
+// alert(length)
+
+
+// var a = prompt("enter any thing")
+// var b = a.charAt(a.length-1)
+// alert(b)
+
+
+
+
+                     //Strings:finding segments
+        
+                     
+// var p = "Pakistani"
+// var q = p.indexof("n")
+// alert("q")
+
+
+
+
+
+// var userName=prompt("Enter your Input: ");
+
+// if(userName.match(/[\@\.\,\!]/)===null){
+//     alert("Valid Username found");
+    
+// }
+// else{
+// alert("INValid Username found");
+
+// }
+
+
+
+
+// var c = "The quick brown fox jumps over the lazy dog";
+// var d = c.match(/the/gi);
+// alert(d)
+
+
+
+
+
+                    //Strings:finding charater
+
+
+                    
+
+// var e = "Pakistani"
+// var f = e.charAt(3)
+// document.write(f)
+
+
+
+
+                 //Strings:replacing character
+
+
+
+// var g = "Hyderabad"
+// var h = g.replace  ("Hyder" , "Islam")
+// document.write(h)
+
+
+// var i = "Ali and Sami are best friends. They play cricket and football together.";
+//  var j = i.replace(/and/g,'&');
+//  document.write(j)
+
+
+
+
+
+                //Rounding Numbers
+
+
+
+// var k = +prompt("enter positive integer");
+// document.write("the number is:" +k+ "<br>" )
+
+// var l = Math.round(k)
+// document.write("round off value is:" +l+ "<br>" )
+
+// var m = Math.floor(k)
+// document.write("floor value is:" +m+ "<br>" )
+
+
+// var n = Math.ceil(k)
+// document.write("floor value is:" +n+ "<br>" )
+
+
+
+
+// var num = parseFloat(prompt("Enter Number"));
+
+//  if (num < 0) {
+//  document.write("Your Number is: " + num + "<br>" + "After Round off: " + Math.round(num) + "<br>" + "Floor Value is: " + Math.floor(num) + "<br>" + "Ceil Value is: " + Math.ceil(num))}
+//  else{
+//  document.write("You did not write the correct information");
+//  }
+
+
+
+                                //Random Numbers
+
+
+
+// var o = Math.random();
+// var p =  (o*6)  +1
+// var q =  Math.floor(p)
+// document.write(q)
+
+
+// var r = Math.random();
+// var s =  (r*2)  +1
+// var t =  Math.floor(s) if (r=1){
+//    alert("tails");
+//         }
+// else{
+// alert("heads");
+    
+//  }
+
+// document.write("2=heads <br> 1=tails")
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+                    //convert strings into integers
+
+                    
+
+// var weight = prompt("Enter Your Weight");
+
+// if (weight === '50' ){
+// weight = parseFloat('50');
+// document.write("Your weight is: " + weight);
+// }
+// else if (weight === '50kgs' ){
+// weight = parseFloat('50');
+// document.write("Your Weight is: " + weight );
+// }
+// else if (weight === '50.2kgs' ){
+// weight = parseFloat('50.2');
+// document.write("Your Weight is: " + weight );
+// }
+// else if (weight === '50.2kilograms' ){
+// weight = parseFloat('50.2');
+// document.write("Your Weight is: " + weight );
+// }
+// else{
+//alert("Write Correct Information")
+// } 
+
+
+
+
+                       //convert strings into number
+
+
+
+        // var u = "472"
+        // var v = Number(u)
+        // alert(v)
+        // var w = typeof(v)
+        // alert(w); 
+        
+        
+var x = "35.36"
+var y = x.toString .replace (".", "")
+document.write(y)
